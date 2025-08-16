ad248ff96d7414bb50860bd242bda6093f5a772e
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-08-16 23:52:49 +0530

    Add bank_integration app and configure middleware in settings.py

--- moneytrack/settings.py
+++ moneytrack/settings.py
@@ -37,9 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'accounts',
+    'dashboard',
+    'transactions',
+    'budgets',
+    'analytics',
+    'shared_ledger',
+    'bank_integration',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
